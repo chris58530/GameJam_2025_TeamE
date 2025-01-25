76b62fab90430a3f0f72332c2bf008e3368e0992
小仔 <71455438+chris58530@users.noreply.github.com>
2025-01-25 20:24:51 +0800

    health bar

--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 public class ScoreUI : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text scoreText;
+
+    [SerializeField]private Image duckHealthImage;
     void OnEnable()
     {
         EventTable.onPlayerScoreChange += UpdateScore;
